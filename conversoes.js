8d92731b1d5d78180c23a4a411973131c8dee7e6
Luis Fernando Peretti <lfperetti@gmail.com>
2023-02-28 17:56:09 -0300

    conversao

--- conversoes.js
+++ conversoes.js
@@ -10,9 +10,26 @@ const numeroString = "456";
 
 // Number()
 // String()
-console.log(numero + numeroString); 
+//console.log(numero + numeroString); 
 
 
 // conversao explicita
 
-console.log(numero + Number(numeroString)); 
+//console.log(numero + Number(numeroString)); 
+var respostaDeTudo = 42
+let idade = 29
+const pi = 3.14
+{
+    var respostaDeTudo = 3.14
+    let idade = 42
+    const pi = 29
+    console.log(respostaDeTudo, idade, pi) // 3.14 42 29
+
+}
+console.log(respostaDeTudo, idade, pi) // 3.14 29 3.14
+
+
+// Variáveis declaradas com var ou let podem ser reatribuídas.
+// Porém, uma variável declarada com let só pode ser usada no mesmo escopo em que está definida.
+// No caso da declaração const, significa que a variável sempre terá uma referência ao mesmo objeto ou valor primitivo,
+// porque essa referência não pode ser alterada. A referência em si é imutável, mas o valor mantido pela variável não se torna imutável.
