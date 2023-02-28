831902ae183174e3b17be5f65e0798f9921578d8
Luis Fernando Peretti <lfperetti@gmail.com>
2023-02-28 17:46:22 -0300

    conversoes

---
+++ conversoes.js
@@ -0,0 +1,18 @@
+// tipo de dado
+// booleanos
+
+// conversao implicita
+const numero = 456;
+const numeroString = "456";
+
+//console.log(numero === numeroString);   // diferente
+//console.log(numero === numeroString);   // 'igual'
+
+// Number()
+// String()
+console.log(numero + numeroString); 
+
+
+// conversao explicita
+
+console.log(numero + Number(numeroString)); 
---
+++ truthy-falsy.js
@@ -0,0 +1,22 @@
+// bolean
+const usuarioLogado = true;
+const contapaga = false;
+
+//console.log(0 == false);
+//console.log("" == false);
+//console.log(1 == true);
+
+// null
+// undefined
+
+let minhaVar;
+let varNull = null;
+
+let numero = 3;
+let texto = "alura";
+
+console.log(typeof minhaVar);
+console.log(typeof varNull);
+
+console.log(typeof numero);
+console.log(typeof texto);
