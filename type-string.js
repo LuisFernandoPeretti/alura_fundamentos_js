fbff91c5eccd6868cceed69b2cdd0cd8df037866
Luis Fernando Peretti <lfperetti@gmail.com>
2023-02-28 14:55:18 -0300

    exemplos da aula1

---
+++ type-string.js
@@ -0,0 +1,16 @@
+const texto1 = 'Olá, mundo!';
+const texto2 = "Olá, mundo!";
+const senha = "senhaSuperSegura456!";
+const StringDeNumeros = "34567";
+
+const citacao = 'Meu nome é ';
+const meuNome = 'Luís';
+
+//console.log(citacao);
+
+// concatenação (+)
+
+console.log(citacao + meuNome);
+
+// template String ou template literal
+
